33e61d316bddca5006547065336534e65c190be5
Ralph Göstenmeier <rg@via-internet.de>
2018-08-04 18:15:07 +0200

    changes

--- README.md
+++ README.md
@@ -1,41 +1,19 @@
-# ionic2-2048
-a 2048 game of ionic2
 
-这是一个使用ionic2框架实现的2048小游戏。
 
-#### 演示
-![2048](https://github.com/HsuanXyz/hsuan.github.io/blob/master/assets/ionic2-2048/2048.gif?raw=true)
 
-#### 已知问题
-在浏览器环境下调试时，滑动事件并不能很好的触发，可能需要在页面上通过按钮控制
+# Install Capacitor support
+    npm install --save @capacitor/cli @capacitor/core
+    npm uninstall --save cordova-plugin-splashscreen
 
-## 获取
+# Build App
+	npm run build
 
-***需要 `ionic` 和 `cordova` ***
+# Add Capacitor
+    npx cap init 2048 com.example.app
+	npx cap add ios
 
-`$ git clone https://github.com/HsuanXyz/ionic2-2048.git`
+    npx cap copy ios
+    npx cap open ios
+  
 
-`$ npm install`
 
-`$ ionic state reset --plugins`
-
-`$ cordova plugin add cordova-sqlite-storage --save`
-## 运行在浏览器
-`$ ionic serve`
-
-## 打包到移动平台
-`$ ionic build android `
-
-`$ ionic build ios`
-
-## 我的环境
-```
-Cordova CLI: 6.4.0
-Ionic Framework Version: 2.0.0-rc.4
-Ionic CLI Version: 2.1.18
-Ionic App Lib Version: 2.1.9
-Ionic App Scripts Version: 0.0.47
-OS: macOS Sierra
-Node Version: v6.9.2
-Xcode version: Xcode 8.2.1 Build version 8C1002
-```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,34 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { Storage } from '@ionic/storage';
-import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-import { SwipeVertical } from '../components/swipe-vertical/swipe-vertical';
-
+import { NgModule, ErrorHandler } from "@angular/core";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { Storage } from "@ionic/storage";
+import { MyApp } from "./app.component";
+import { AboutPage } from "../pages/about/about";
+import { ContactPage } from "../pages/contact/contact";
+import { HomePage } from "../pages/home/home";
+import { TabsPage } from "../pages/tabs/tabs";
+import { SwipeVertical } from "../components/swipe-vertical/swipe-vertical";
 
 export function provideStorage() {
-  return new Storage(['sqlite', 'websql', 'indexeddb'], { name: '__2048game' })
+	return new Storage(["sqlite", "websql", "indexeddb"], {
+		name: "__2048game"
+	});
 }
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    SwipeVertical
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},{ provide: Storage, useFactory: provideStorage }]
+	declarations: [
+		MyApp,
+		AboutPage,
+		ContactPage,
+		HomePage,
+		TabsPage,
+		SwipeVertical
+	],
+	imports: [IonicModule.forRoot(MyApp)],
+	bootstrap: [IonicApp],
+	entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
+	providers: [
+		{ provide: ErrorHandler, useClass: IonicErrorHandler },
+		{ provide: Storage, useFactory: provideStorage }
+	]
 })
 export class AppModule {}
